abea077837a774737f6a98e84dfe3753aef84b2a
DALVAN SIEBRA VANDERLEI <DALVAN0471@DIGITALCOLLEGE.LOCAL>
2023-11-18 16:30:28 -0300

    Inicio do Banner

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,22 +1,34 @@
 import ImgsComponents from "../../ImgsComponents/ImgsComponents";
 import ImgBanner from "../../img/banner2.webp"
-import RedeSocial from "../../img/Group16.png"
+import Facebook from "../../img/facebook2.svg"
+import Linkedin from "../../img/linkedin.svg"
+import GitHub from "../../img/gitHub.svg"
+import Instagran from "../../img/instagram.svg"
 import "./styles.css"
+import LinkHeader from "../Header/links";
+import Button from "../Button/Button";
 
 
 export default function Banner (){
     return(
         <>
             <div className="banner"> 
-                <ImgsComponents src={ImgBanner} alt="Imagem" />
+                
                 <div>
                     <p>Hi, It's me</p>
                     <h1>MD RIEAD MIA</h1>
                     <h3>And I'm a Frontend Developer</h3>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eosdolorum ullam molestias autem deleniti iure earum!</p>
-                    <ImgsComponents src={RedeSocial} alt="Redes Sociais" />
-                </div>
+                 
+                    <LinkHeader href="#" text={<><><ImgsComponents src={Facebook} alt="Redes Sociais" /></></>} />
+                    <LinkHeader href="#" text={<><><ImgsComponents src={Linkedin} alt="Redes Sociais" /></></>} />
+                    <LinkHeader href="#" text={<><><ImgsComponents src={GitHub} alt="Redes Sociais" /></></>} />
+                    <LinkHeader href="#" text={<><><ImgsComponents src={Instagran} alt="Redes Sociais" /></></>} />
 
+                    <Button text="Download CV" />
+                </div>
+                
+                <ImgsComponents className="ImgBaner" src={ImgBanner} alt="Imagem" />
             </div>    
         </>
     )
