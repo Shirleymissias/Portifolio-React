63ed50c8f86a50d2dd43399f53b848026563061a
shirley <shirleymissias20@gmail.com>
2024-01-19 01:18:21 -0300

    Conclusão do Contact

--- src/views/Contact/index.jsx
+++ src/views/Contact/index.jsx
@@ -8,35 +8,78 @@ import Send from "../../assets/img/send.svg"
 
 export default function Contact() {
     return (
-        <>
-            <div className="conatctRoot">
-                <h1>Contact</h1>
-                <div className="contactInfo">
-                    <div>
-                        <ImgsComponents src={Phone} alt="ContactPhone" className="ContactImg" />
-                        <p>+8801863-931220</p>
-                        <p>+8801567-953483</p>        
-                    </div>
-                    <div>
-                        <ImgsComponents src={Email} alt="ContactEmail" className="ContactImg" />
-                        <p>mdriead.bd@gmail.com</p>       
-                    </div>
-                    <div>
-                        <ImgsComponents src={Send} alt="ContactEmail" className="ContactImg" />
-                        <p>Zirabo, Ashulia, Savar, Dhaka</p>   
-                    </div>
-                </div>
+      <>
+        <div className="conatctRoot">
+          <h1>Contact</h1>
+          <div className="contactConteudo">
+            <div className="contactInfo">
+              <div className="contactItem">
+                <ImgsComponents
+                  src={Phone}
+                  alt="ContactPhone"
+                  className="ContactImg"
+                />
                 <div>
-                    <form action="">
-                        <input type="text" placeholder="Full Name" className="contactText" />
-                        <input type="email" placeholder="Email Adreess" className="contactText" />
-                        <input type="text" placeholder="Phone Number" className="contactNum"  />
-                        <input type="text" placeholder="Subject" className="contactNum"  />
-                        <input type="text" placeholder="Your Message" className="contactMessage"  />
-                        <button>Send</button>
-                    </form>
+                  <p>+8801863-931220</p>
+                  <p>+8801567-953483</p>
                 </div>
+              </div>
+              <div className="contactItem">
+                <ImgsComponents
+                  src={Email}
+                  alt="ContactEmail"
+                  className="ContactImg"
+                />
+                <p>mdriead.bd@gmail.com</p>
+              </div>
+              <div className="contactItem">
+                <ImgsComponents
+                  src={Send}
+                  alt="ContactEmail"
+                  className="ContactImg"
+                />
+                <p>Zirabo, Ashulia, Savar, Dhaka</p>
+              </div>
             </div>
-        </>
-    )
+
+            <div className="contactForm">
+              <form action="">
+                <input
+                  type="text"
+                  placeholder="Full Name"
+                  className="contactText"
+                />
+                <input
+                  type="email"
+                  placeholder="Email Adreess"
+                  className="contactText"
+                />
+                <input
+                  type="text"
+                  placeholder="Phone Number"
+                  className="contactNum contactNumTwo"
+                />
+                <input
+                  type="text"
+                  placeholder="Subject"
+                  className="contactNum"
+                />
+                {/* <input
+                    type="text"
+                    placeholder="Your Message"
+                    className="contactMessage"
+                /> */}
+                <textarea
+                  name=""
+                  id="msg"
+                  placeholder="Your Message"
+                  className="contactMessage"
+                ></textarea>
+                <button className="contactButton">Send</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </>
+    );
 }
