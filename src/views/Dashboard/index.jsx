6d905be9413c47ceebbc92cc67444e61fe9af556
shirley.souza <shirleymissias20@gmail.com>
2024-03-23 11:58:59 -0300

    Criação da requisição para salvar as linguagens e porcentagens

--- src/views/Dashboard/index.jsx
+++ src/views/Dashboard/index.jsx
@@ -1,25 +1,63 @@
 import "../../styles.globals.css"
-import { useEffect } from "react";
+import "../Contact/style.css"
+import app from "../../App"
+import { useEffect, useState } from "react";
+
+
+
+
 
 export default function DashBoard() {
-    return (
-      <>
-    
-    function get(params) {
-        
-    }
 
-       <form action="">
-            <div>
-                Linguagem:
-                <input type="text"  name="language" placeholder="language"/>
+  const [dataForm, setDataForm] = useState ({
+    language: "",
+    percentage: "",
+  })
+  
+  const inputHandle = (event) => {
+    setDataForm({...dataForm, [event.target.name]: event.target.value })
+    
+  }
+
+  const addLanguage = (event) => {
+    event.preventDefault()
+    console.log(dataForm)
+    fetch("http://localhost:3000/programming-languages", {
+
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(dataForm),
+    })
+
+    .then((response) => response.json())
+    .then((response) => {
+      console.log(response)
+    })
+  }
+
+
+    return (
+      <>  
+      <div className="conatctRoot"> 
+        <h3>Cadastrar linguagem de programação</h3>
+        <div className="contactForm">
+          <form action="">
+            <div className="input-form">
+              <input type="text" name="language" placeholder="Language" onChange={inputHandle} className="contactText"/>
             </div>
-            <div>
-                Porgentagem:
-                <input type="number"  name="percentege" placeholder="Percentege" />
+
+            <div className="input-form">
+              <input type="text" name="percentage" placeholder="Percentege" onChange={inputHandle} className="contactText"/>
             </div>
-            <button>Cadastrar</button>
-       </form>
+
+            <div className="input-form">
+              <button className="add" onClick={addLanguage}>Cadastrar</button>
+            </div>
+          </form>
+        </div>
+        </div>     
       </>
     );
   }
